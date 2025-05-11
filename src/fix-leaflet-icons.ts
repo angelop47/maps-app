7a39961852cd941cbc6564174bd19c1ade9b8a86
angelop47 <pescettoangelo@gmail.com>
2025-05-11 10:52:42 -0300

    .

--- src/fix-leaflet-icons.ts
+++ src/fix-leaflet-icons.ts
@@ -1,10 +1,10 @@
-import L from 'leaflet';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
-// Eliminar la referencia a la URL por defecto de los íconos
-delete (L.Icon.Default.prototype as any)._getIconUrl;
+// Sobrescribir la función de íconos solo si existe
+(L.Icon.Default.prototype as unknown as { _getIconUrl?: () => string })._getIconUrl = undefined;
 
 // Configurar los nuevos íconos
 L.Icon.Default.mergeOptions({
