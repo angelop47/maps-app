24045ea8a7056ac83bc83d686c504b8ddc5fae99
angelop47 <pescettoangelo@gmail.com>
2025-05-11 18:17:07 -0300

    data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,11 +1,19 @@
+import publicIcon from "@/assets/public.png";
+import trashIcon from "@/assets/recycling-bin.png";
+import ecoIcon from "@/assets/eco-center.png";
+import culturalIcon from "@/assets/cultural.png";
+import dangerIcon from "@/assets/danger.png";
+
+export type CategoryId = "public" | "trash" | "eco-center" | "cultural" | "danger";
+
 export type Location = {
-  id: string
-  name: string
-  description: string
-  address: string
-  lat: number
-  lng: number
-  type: "trash" | "public"
+  id: string;
+  name: string;
+  description: string;
+  address: string;
+  lat: number;
+  lng: number;
+  type: CategoryId;
 }
 
 export const locations: Location[] = [
@@ -100,3 +108,12 @@ export const locations: Location[] = [
     type: "public",
   },
 ]
+
+
+export const CATEGORIES: { id: CategoryId; name: string; icon: string; }[] = [
+  { id: "public", name: "Lugar público", icon: publicIcon },
+  { id: "trash", name: "Contenedor de basura", icon: trashIcon },
+  { id: "eco-center", name: "Centro ecológico", icon: ecoIcon },
+  { id: "cultural", name: "Sitio cultural", icon: culturalIcon},
+  { id: "danger", name: "Zona peligrosa", icon: dangerIcon },
+];
