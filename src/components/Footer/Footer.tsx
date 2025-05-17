c2a669a5d10d2c86148a3fa671225fc19a35fc57
angelop47 <pescettoangelo@gmail.com>
2025-05-17 10:04:34 -0300

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,16 +1,15 @@
 const Footer = () => {
   return (
     <footer>
-      <div className="bg-neutral-200 p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200 text-sm">
+      <div className="bg-white p-4 text-center text-neutral-700 dark:bg-neutral-700 dark:text-neutral-200 text-sm">
         <p>
-          © 2023 Copyright:
+          © {new Date().getFullYear()} Copyright:
           <a
             className="ml-1 text-neutral-800 dark:text-neutral-400"
-            href="https://tw-elements.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            TW Elements
+            Angelo Pescetto
           </a>
         </p>
         <p className="mt-1">
